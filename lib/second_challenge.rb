e114b6e332d907349b5f118137f729011ae2b754
Alexander Truong <alexander.m.truong@gmail.com>
2018-09-29 20:06:02 +0000

    automatically backed up by learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,15 +1,17 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-     :patriarch => 
-       {},
-     :matriarch => {}
-     :hero => {}
-     :hero_friends => []
-    :capulet => {}
-      :patriarch {
-      #your key/value pairs here!
-      }}
-
+      :patriarch => {},
+      :matriarch => {},
+      :hero => {},
+      :hero_friends => []
+    },
+    :capulet => {
+      :patriarch => {},
+      :matriarch => {},
+      :heroine => {},
+      :heroine_friends => []
+    }
+  }
   
 end
